9b16618242082799224f3a6f0a8ab8ab1a05e329
Chris Grandin <chris.grandin@gmail.com>
2020-04-27 12:47:57 -0700

    The project is now a working package * The doc directory is included in the git repo but is not part of the package * Passes check() * Document builds * All non-git stuff is now located outside project dir to avoid confusion

--- R/model-setup.R
+++ R/model-setup.R
@@ -1,32 +1,45 @@
-# These assignments to NULL are required to avoid check() warnings.
-# The function model_setup() must be run to assign these variables to values.
-nongit_dir <- NULL
-models_dir <- NULL
-base_model_dir <- NULL
-base_model_dir_full <- NULL
-regen_rds_files <- NULL
-sens_models_dirs <- NULL
-sens_models <- NULL
-unique_models_dirs <- NULL
-unique_models_dirs_full <- NULL
-unique_models <- NULL
-base_model <- NULL
+#' Set main directories for the project
+#'
+#' @param nongit_dir The directory containing non-version-controlled things
+#' such as data, model runs from iscam, and reference materials
+#' @param models_dir The name of the directory in which the iscam model
+#' directories are located
+#' @param base_model_dir Name of the base model directory
+#'
+#' @return A list of 4 items, the first three are the same as the input arguments
+#' and the 4th is the full path of the base model directory
+#' @export
+set_dirs <- function(nongit_dir = file.path("..", "arrowtooth-nongit"),
+                     models_dir = file.path(nongit_dir, "models"),
+                     base_model_dir = "01-base"){
+  list(nongit_dir = nongit_dir,
+       models_dir = models_dir,
+       base_model_dir = base_model_dir,
+       base_model_dir_full = file.path(models_dir, base_model_dir))
+}
 
-model_setup <- function(){
-  nongit_dir <<- file.path("..", "arrowtooth-nongit")
-  models_dir <<- file.path(nongit_dir, "models")
+#' Load models and set up the directory names
+#'
+#' @param main_dirs A ilst of four, with the same names as the output
+#' from [set_dirs()]
+#' @param overwrite_rds_files Logical. TRUE to overwrite the model RDS files
+#'
+#' @return A list of two items, the base_model and the list of sensitivity models,
+#' which itself it composed of lists of groups of sensitivities which are to be compared with
+#' each other in the document. This simplifies plotting and table functions.
+#' @export
+model_setup <- function(main_dirs = set_dirs(),
+                        overwrite_rds_files = FALSE){
 
-  base_model_dir <<- "01-base"
-  base_model_dir_full <<- file.path(models_dir, base_model_dir)
-
-  if(!exists("regen_rds_files")){
-    regen_rds_files <<- FALSE
-  }
+  nongit_dir <- main_dirs$nongit_dir
+  models_dir <- main_dirs$models_dir
+  base_model_dir <- main_dirs$base_model_dir
+  base_model_dir_full <- main_dirs$base_model_dir_full
 
   # A list of lists of sensitivity groups. Any given sensitivity can appear in more than one group.
   # Do not include base model in this list or it will appear twice on plots
   # The base model name is added to each group with the map..prepend pipe at the end
-  sens_models_dirs <<- list(
+  sens_models_dirs <- list(
     c("02-sigma-0.1",
       "03-estimated-total-variance"),
     c("04-tau-1.0",
@@ -40,28 +53,31 @@ model_setup <- function(){
     c("12-fix-trawl-sel-amat",
       "13-fix-trawl-sel-6years")
   ) %>%
-    map(~{prepend(.x, base_model_dir, before = 1)})
+    map(~{append(.x, base_model_dir, after = 0)})
 
-  unique_models_dirs <<- sens_models_dirs %>%
+  unique_models_dirs <- sens_models_dirs %>%
     flatten() %>%
     unique() %>%
     map_chr(.f = ~{.x})
 
-  unique_models_dirs_full <<- file.path(models_dir, unique_models_dirs)
+  unique_models_dirs_full <- file.path(models_dir, unique_models_dirs)
 
-  nul <- map(unique_models_dirs_full, ~{create_rds_file(.x, overwrite = regen_rds_files)})
+  nul <- map(unique_models_dirs_full, ~{gfiscamutils::create_rds_file(.x, overwrite = overwrite_rds_files)})
 
   # This ensures that each unique model is loaded only once, even if it is in multiple
   # sensitivity groups
-  unique_models <<- map(unique_models_dirs_full, ~{load_rds_file(.x)}) %>%
+  unique_models <- map(unique_models_dirs_full, ~{load_rds_file(.x)}) %>%
     set_names(unique_models_dirs)
 
-  base_model <<- unique_models[[match(base_model_dir, unique_models_dirs)]]
+  base_model <- unique_models[[match(base_model_dir, unique_models_dirs)]]
   # sens_models is a list of lists of sensitivities of the same structure as sens_models_dirs.
   # the base_model is first in each sensitivity group list
-  sens_models <<- map(sens_models_dirs, ~{
+  sens_models <- map(sens_models_dirs, ~{
     map(.x, ~{
       unique_models[[match(.x, unique_models_dirs)]]
     })
   })
+
+  list(base_model = base_model,
+       sens_models = sens_models)
 }
