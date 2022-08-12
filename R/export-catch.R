0d6618ae2146a467c392ca887850cf12e1ae9741
Chris Grandin <chris.grandin@gmail.com>
2022-08-12 10:47:55 -0700

    Add some export data stuff

--- R/export-catch.R
+++ R/export-catch.R
@@ -10,9 +10,12 @@
 #' `FALSE`, return a list of two data frames
 #' @param fns A vector of two filenames
 #' @param middle_text A vector of middle column data to insert in the output
+#' @param ... Arguments passed to [gfplot::tidy_catch()] and in turn, to
+#' [gfplot::set_fishing_year()]
 #'
 #' @return Nothing is `write_files` is `TRUE`. A list of two data frames if
 #' `write_files` is `FALSE`
+#' @importFrom lubridate month day
 #' @export
 export_catch <- function(ct,
                          areas = c("3[CD]+", "5[ABCDE]+"),
@@ -20,18 +23,57 @@ export_catch <- function(ct,
                          divisor = 1e6,
                          digits = 4,
                          write_files = TRUE,
+                         single_fleet = FALSE,
                          fns = c(paste0("catch-ft-", Sys.Date(), ".txt"),
                                  paste0("catch-ss-", Sys.Date(), ".txt")),
                          middle_text = c(gear = 1,
                                          group = 1,
                                          area = 1,
                                          sex = 0,
-                                         type = 1)){
+                                         type = 1),
+                         ...){
 
   options(pillar.sigfig = 7)
-  # Catch by fleet
-  catch_ft <- extract_fleet_catch(ct) |>
-    tidy_catch(areas = areas)|>
+
+  if(single_fleet){
+    catch_all <- ct |>
+      tidy_catch(areas = areas, ...) |>
+      group_by(year) |>
+      summarize(round(sum(value / divisor), digits)) |>
+      filter(year %in% years) |>
+      complete(year = years) |>
+      rename(catch = 2)
+    catch_all[is.na(catch_all)] <- 0
+    if(!is.null(middle_text)){
+      middle_df <- vec2df(middle_text, nms = names(middle_text))
+      middle_df <- do.call("rbind",
+                           replicate(nrow(catch_all),
+                                     middle_df,
+                                     simplify = FALSE))
+      catch_all <- bind_cols(catch_all[1], middle_df, catch_all[2])
+    }
+
+    if(write_files){
+      nongit_dir <- file.path(dirname(here()), "arrowtooth-nongit")
+      dir.create(file.path(nongit_dir, "data-output"), showWarnings = FALSE)
+      fn <- file.path(nongit_dir,
+                      file.path("data-output",
+                                fns[1]))
+
+      write.table(catch_all, fn,
+                  quote = FALSE,
+                  row.names = FALSE)
+      message("Created file ", fn)
+      return(invisible())
+    }else{
+      return(list(catch_all))
+    }
+  }
+
+  # Freezer trawler catch -----------------------------------------------------
+  catch_ft <-
+    extract_fleet_catch(ct) |>
+    tidy_catch(areas = areas, ...) |>
     group_by(year) |>
     summarize(round(sum(value / divisor), digits)) |>
     filter(year %in% years) |>
@@ -47,8 +89,9 @@ export_catch <- function(ct,
     catch_ft <- bind_cols(catch_ft[1], middle_df, catch_ft[2])
   }
 
+  # Shoreside catch -----------------------------------------------------------
   catch_ss <- extract_fleet_catch(ct, include = FALSE) |>
-    tidy_catch(areas = areas)|>
+    tidy_catch(areas = areas, ...) |>
     group_by(year) |>
     filter(year %in% years) |>
     summarize(round(sum(value / divisor), digits)) |>
--- R/export-iscam-data.R
+++ R/export-iscam-data.R
@@ -1,6 +1,7 @@
 #' Export all iSCAM data for pasting into the iSCAM data files
 #'
-#' @param dat Output from [gfdata::cache_pbs_data()]
+#' @param dat Output from [gfdata::cache_pbs_data()]. Make sure to set the
+#' argument `unsorted_only = FALSE` for arrowtooth
 #'
 #' @return Nothing, creates several files for cutting/pasting into iSCAM
 #' data files
@@ -19,4 +20,37 @@ export_iscam_data <- function(dat){
   catch_ss <- extract_fleet_catch(dat$catch, include = FALSE)
   export_age_comps(catch_ft, comm_ft, gear_num = 1, fn_append = "freezer-trawlers")
   export_age_comps(catch_ss, comm_ss, gear_num = 2, fn_append = "shoreside")
+
+  survey_sets <- dat$survey_sets |>
+    filter(major_stat_area_code %in% c("03","04", "05", "06", "07", "08", "09"))
+  survey_samples <- dat$survey_samples |>
+    filter(major_stat_area_code %in% c("03","04", "05", "06", "07", "08", "09"))
+  export_age_comps(survey_sets,
+                   survey_samples,
+                   type = "survey",
+                   gear_num = 3,
+                   surv_series_name = "SYN QCS",
+                   fn_append = "qcs")
+
+  export_age_comps(survey_sets,
+                   survey_samples,
+                   type = "survey",
+                   gear_num = 5,
+                   surv_series_name = "SYN HS",
+                   fn_append = "hs")
+
+  export_age_comps(survey_sets,
+                   survey_samples,
+                   type = "survey",
+                   gear_num = 6,
+                   surv_series_name = "SYN WCVI",
+                   fn_append = "wcvi")
+
+  export_age_comps(survey_sets,
+                   survey_samples,
+                   type = "survey",
+                   gear_num = 7,
+                   surv_series_name = "SYN WCHG",
+                   fn_append = "wchg")
+
 }
