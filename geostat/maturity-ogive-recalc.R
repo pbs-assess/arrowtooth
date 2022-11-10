68e33b601f2020a08f207e027a705134c2d54507
ecophilina <paenglis@lakeheadu.ca>
2022-11-10 15:17:25 -0800

    new ogive plot

--- geostat/maturity-ogive-recalc.R
+++ geostat/maturity-ogive-recalc.R
@@ -1,5 +1,8 @@
 # optim maturity
 
+# library(viridis)
+library(patchwork)
+
 all_samples <- bind_rows(survey_samples, commercial_samples)
 
 mat_model <- function(par, age, prop_mature) {
@@ -52,29 +55,29 @@ save_ogive_dataframe <- function(data, maturities_kept) {
 `Synoptic only` <- survey_samples_syn %>% filter(age < 41)
 `All samples` <- all_samples %>% filter(age < 41)
 
-d1 <- save_ogive_dataframe(`synoptic only`,
+d1 <- save_ogive_dataframe(`Synoptic only`,
   maturities_kept = c(1, 2, 3, 4, 5, 6, 7)
 )
-d2 <- save_ogive_dataframe(`all samples`,
+d2 <- save_ogive_dataframe(`All samples`,
   maturities_kept = c(1, 2, 3, 4, 5, 6, 7)
 )
-d3 <- save_ogive_dataframe(`all samples`,
+d3 <- save_ogive_dataframe(`All samples`,
   maturities_kept = c(1, 2, 3, 4, 5, 6)
 )
-d4 <- save_ogive_dataframe(`all samples`,
+d4 <- save_ogive_dataframe(`All samples`,
   maturities_kept = c(1, 4, 5, 6)
 )
-d5 <- save_ogive_dataframe(`synoptic only`,
-  maturities_kept = c(1, 4, 5, 6)
-)
+# d5 <- save_ogive_dataframe(`Synoptic only`,
+#   maturities_kept = c(1, 4, 5, 6)
+# )
 
-mat_dat <- bind_rows(d1, d2, d3, d4, d5) %>% mutate(
+mat_dat <- bind_rows(d1, d2, d3, d4) %>% mutate(
   model = case_when(
     maturities_kept == paste0(deparse(c(1, 2, 3, 4, 5, 6, 7))) ~ paste(data),
-    # maturities_kept == paste0(deparse(c(1, 2, 3, 4, 5, 6))) ~ paste(data, "\n(resting removed)"),
-    # maturities_kept == paste0(deparse(c(1, 4, 5, 6))) ~ paste(data, "\n(maturing, developing \nand resting removed)")
-    maturities_kept == paste0(deparse(c(1, 2, 3, 4, 5, 6))) ~ "Resting removed",
-    maturities_kept == paste0(deparse(c(1, 4, 5, 6))) ~ "Maturing, developing \nand resting removed"
+    maturities_kept == paste0(deparse(c(1, 2, 3, 4, 5, 6))) ~ paste(data, "\n(resting removed)"),
+    maturities_kept == paste0(deparse(c(1, 4, 5, 6))) ~ paste(data, "\n(maturing, developing \nand resting removed)")
+    # maturities_kept == paste0(deparse(c(1, 2, 3, 4, 5, 6))) ~ "Resting removed",
+    # maturities_kept == paste0(deparse(c(1, 4, 5, 6))) ~ "Maturing, developing \nand resting removed"
   ),
   sex = ifelse(sex == 1L, "Male", "Female")
 )
@@ -82,125 +85,117 @@ unique(mat_dat$model)
 mat_dat$model <- factor(mat_dat$model, levels = c(
   "Synoptic only",
   "All samples",
-  # "all samples \n(resting removed)",
-  # "all samples \n(maturing, developing \nand resting removed)"
+  "All samples \n(resting removed)",
+  "All samples \n(maturing, developing \nand resting removed)"
+  # "Resting removed",
+  # "Maturing, developing \nand resting removed"
+  # "Synoptic only \n(maturing, developing \nand resting removed)"
+), labels = c(
+  "Synoptic only",
+  "All samples",
+  # "All samples \n(resting removed)",
+  # "All samples \n(maturing, developing \nand resting removed)"
   "Resting removed",
   "Maturing, developing \nand resting removed"
-  # "synoptic only \n(maturing, developing \nand resting removed)"
+  # "Synoptic only \n(maturing, developing \nand resting removed)"
 ))
 
 mat_dat$sex <- factor(mat_dat$sex, levels = c("Male", "Female"))
 age <- 1:40
 
-cols <- c( "#FC8D62", "#0096FF", "#4682B4", "black", "red")
+# female_cols <- c("#FC8D62", "#0096FF", "#4682B4", "black", "red")
+# female_cols <- plasma(4)
+# female_cols <- inferno(5)
 
-p1 <- mat_dat %>%
-  filter(sex == "Male") %>%
-  ggplot() + geom_point(aes(age, prop_mature, colour = model),
-    # shape = 1,
-    alpha = 0.5
+library(RColorBrewer)
+# female_cols <- brewer.pal(n = 6, name = "Reds")
+female_cols <- brewer.pal(n = 8, name = "YlOrRd")
+female_cols <- female_cols[5:8]
+female_cols <- rev(female_cols)
+
+# male_cols <- brewer.pal(n = 6, name = "Blues")
+male_cols <- brewer.pal(n = 6, name = "GnBu")
+male_cols <- male_cols[3:6]
+male_cols <- rev(male_cols)
+
+
+
+
+plot_bridging_mat_ogives <- function(mat_dat,
+                                     plot_sex = "Female",
+                                     col_vec = female_cols,
+                                     ogive_size = 1.3,
+                                     ogive_alpha = 0.4) {
+
+  models <- unique(mat_dat$model)
+
+  p <- mat_dat %>%
+    filter(sex == !!plot_sex) %>%
+    ggplot() +
+    geom_point(aes(age, prop_mature, colour = model),
+      # shape = 1,
+      alpha = 0.7
+    ) +
+    scale_color_manual(values = col_vec, name = "Model") +
+    ylab("Proportion mature") +
+    xlab("Age") +
+    ggtitle(plot_sex) +
+    ggsidekick::theme_sleek()
+
+
+  add_ogive <- function(p, i){
+  p + geom_function(
+    data = NULL,
+    size = ogive_size,
+    alpha = ogive_alpha,
+    # lty="dashed",
+    fun = function(x) {
+      plogis(
+        x,
+        mat_dat[mat_dat$model == models[i] & mat_dat$sex == plot_sex, ]$age50mat[1],
+        mat_dat[mat_dat$model == models[i] & mat_dat$sex == plot_sex, ]$sd50mat[1]
+      )
+    }, colour = col_vec[i], inherit.aes = FALSE
   ) +
-  # geom_function(fun = ~ plogis(.x, .$age50mat[1], .$sd50mat[1])) +
-  geom_function(fun = function(x) {
-    plogis(x,
-      mat_dat[mat_dat$model == "synoptic only" & mat_dat$sex == "Male", ]$age50mat[1],
-      mat_dat[mat_dat$model == "synoptic only" & sex == 1, ]$sd50mat[1]
+    geom_vline(
+      data = NULL,
+      size = ogive_size / 2, lty = "dashed", alpha = ogive_alpha, xintercept =
+        mat_dat[mat_dat$model == models[i] &
+                  mat_dat$sex == plot_sex, ]$age50mat[1], colour = col_vec[i]
     )
-  }, colour = cols[1], inherit.aes = FALSE) +
-  geom_function(fun = function(x) {
-    plogis(x,
-           mat_dat[mat_dat$model == "all samples" & mat_dat$sex == "Male", ]$age50mat[1],
-           mat_dat[mat_dat$model == "all samples" & sex == 1, ]$sd50mat[1]
-    )
-  }, colour = cols[2], inherit.aes = FALSE) +
-  geom_function(fun = function(x) {
-    plogis(x,
-           mat_dat[mat_dat$model == "all samples \n(resting removed)" & mat_dat$sex == "Male", ]$age50mat[1],
-           mat_dat[mat_dat$model == "all samples \n(resting removed)" & sex == 1, ]$sd50mat[1]
-    )
-  }, colour = cols[3], inherit.aes = FALSE) +
-  geom_function(fun = function(x) {
-    plogis(x,
-           mat_dat[mat_dat$model == "all samples \n(maturing, developing \nand resting removed)"
-                   & mat_dat$sex == "Male", ]$age50mat[1],
-           mat_dat[mat_dat$model == "all samples \n(maturing, developing \nand resting removed)"
-                   & sex == 1, ]$sd50mat[1]
-    )
-  }, colour = cols[4], inherit.aes = FALSE) +
-  # geom_function(fun = function(x) {
-  #   plogis(x,
-  #          mat_dat[mat_dat$model == "synoptic only \n(maturing, developing \nand resting removed)"
-  #                  & mat_dat$sex == "Male", ]$age50mat[1],
-  #          mat_dat[mat_dat$model == "synoptic only \n(maturing, developing \nand resting removed)"
-  #                  & sex == 1, ]$sd50mat[1]
-  #   )
-  # }, colour = cols[5], inherit.aes = FALSE) +
-  # # scale_color_viridis_d(direction = -1) +
-  scale_color_manual(values = cols) +
-  ylab("Proportion mature") +
-  xlab("Age") +
-  # facet_grid(~sex) +
-  ggtitle("Male") +
-  ggsidekick::theme_sleek() +
-  theme(legend.position = "none",
-        axis.title.x = element_blank())
+    }
+
+  for (i in seq_along(models)){
+    p <- p %>% add_ogive(i)
+  }
+
+  p
+}
+
+p1 <- plot_bridging_mat_ogives(mat_dat, plot_sex = "Male", col_vec = male_cols)
+# + theme(
+#   legend.title = element_blank())
+p2 <- plot_bridging_mat_ogives(mat_dat, plot_sex = "Female", col_vec = female_cols) +
+  theme(
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    axis.title.y = element_blank())
+
+# xlab <- p1$labels$x
+
+xlab <- ggplot(data.frame(l = "Age", x = 13, y = 1)) +
+  geom_text(aes(x, y, label = l))+
+  theme_void()
 
 
-p2 <- mat_dat %>%
-  filter(sex == "Female") %>%
-  ggplot() + geom_point(aes(age, prop_mature, colour = model),
-                        # shape = 1,
-                        alpha = 0.5
-  ) +
-  # geom_function(fun = ~ plogis(.x, .$age50mat[1], .$sd50mat[1])) +
-  geom_function(fun = function(x) {
-    plogis(x,
-           mat_dat[mat_dat$model == "synoptic only" & mat_dat$sex == "Female", ]$age50mat[1],
-           mat_dat[mat_dat$model == "synoptic only" & sex == 1, ]$sd50mat[1]
-    )
-  }, colour = cols[1], inherit.aes = FALSE) +
-  geom_function(fun = function(x) {
-    plogis(x,
-           mat_dat[mat_dat$model == "all samples" & mat_dat$sex == "Female", ]$age50mat[1],
-           mat_dat[mat_dat$model == "all samples" & sex == 1, ]$sd50mat[1]
-    )
-  }, colour = cols[2], inherit.aes = FALSE) +
-  geom_function(fun = function(x) {
-    plogis(x,
-           mat_dat[mat_dat$model == "all samples \n(resting removed)" & mat_dat$sex == "Female", ]$age50mat[1],
-           mat_dat[mat_dat$model == "all samples \n(resting removed)" & sex == 1, ]$sd50mat[1]
-    )
-  }, colour = cols[3], inherit.aes = FALSE) +
-  geom_function(fun = function(x) {
-    plogis(x,
-           mat_dat[mat_dat$model == "all samples \n(maturing, developing \nand resting removed)"
-                   & mat_dat$sex == "Female", ]$age50mat[1],
-           mat_dat[mat_dat$model == "all samples \n(maturing, developing \nand resting removed)"
-                   & sex == 1, ]$sd50mat[1]
-    )
-  }, colour = cols[4], inherit.aes = FALSE) +
-  # geom_function(fun = function(x) {
-  #   plogis(x,
-  #          mat_dat[mat_dat$model == "synoptic only \n(maturing, developing \nand resting removed)"
-  #                  & mat_dat$sex == "Female", ]$age50mat[1],
-  #          mat_dat[mat_dat$model == "synoptic only \n(maturing, developing \nand resting removed)"
-  #                  & sex == 1, ]$sd50mat[1]
-  #   )
-  # }, colour = cols[5], inherit.aes = FALSE) +
-  ## scale_color_viridis_d(direction = -1) +
-  scale_color_manual(values = cols, name = "Model") +
-  ylab("Proportion mature") +
-  xlab("Age") +
-  # facet_grid(~sex) +
-  ggtitle("Female") +
-  ggsidekick::theme_sleek() +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.title = element_blank())
+(p1 + p2 )/xlab +
+  plot_layout(heights = c(20, 1.5)) & coord_cartesian(expand = F, xlim = c(0, 26)) & theme(
+  legend.title = element_blank(),
+  axis.title.x = element_blank(),
+  legend.position = c(0.7, 0.25)
+)
 
-library(patchwork)
-
-p1 + p2 + plot_layout()
+ggsave("maturities.png", width = 8, height = 3.5)
 
 # # all synoptic survey data
 # mat_a <- plogis(age, 5.5622846, 0.9170395)
